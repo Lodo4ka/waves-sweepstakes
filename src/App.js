b8e37b97b820e6c6107b78938f4c6f5608d3bd63
максим новиков <maksimnovikov@maksims-Air.Dlink>
2022-08-17 09:23:12 +0300

    temp 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+// import {
+//   signer
+// } from './waves-blockchain';
+import {
+  useEffect
+} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  useEffect(() => {
+    (async () => {
+      const txData = {
+        type: 4,
+        data: {
+          amount: {
+            assetId: 'WAVES',
+            tokens: '0.5',
+          },
+          fee: {
+            assetId: 'WAVES',
+            tokens: '0.001',
+          },
+          recipient: '3N4boZRUJ2LxY5jLyp6hmxGktqnawQidu6H',
+        },
+      };
+      // eslint-disable-next-line no-undef
+      const data = await KeeperWallet.signTransaction(txData)
+      console.log(data);
+      // const user = await signer.login();
+      // console.log(user);
+      // const balances = await signer.getBalance();
+      // console.log(balances);
+      // const result = await signer
+      //   .transfer({
+      //     amount: 10**4,
+      //     recipient: '3N4boZRUJ2LxY5jLyp6hmxGktqnawQidu6H',
+      //     assetId: "WAVES",
+      //   })
+      //   .broadcast();
+      //   console.log(result);
+      //   .broadcast(); // Promise will resolved after user sign and node response
+      //   console.log(broadcastedTransfer);
+
+      // const [signedTransfer] = await signer
+      //   .transfer({
+      //     amount: 100000000,
+      //     recipient: 'alias:T:merry'
+      //   }) // Transfer 1 WAVES to alias merry
+      //   .sign(); // Promise will resolved after user sign
+      //   console.log(signedTransfer);
+
+      // await signer.invoke({
+      //   dApp: '3N4boZRUJ2LxY5jLyp6hmxGktqnawQidu6H',
+      //   call: { function: 'helloWorld', args: '' },
+      // })
+      // .sign();
+    })()
+  }, []);
+
+  return ( <div> </div>
   );
 }
 
---
+++ src/waves-blockchain.js
@@ -0,0 +1,18 @@
+// import { Signer } from '@waves/signer';
+// import { ProviderSeed } from '@waves/provider-seed';
+// import { libs, } from '@waves/waves-transactions';
+// import { ProviderWeb } from '@waves.exchange/provider-web';
+// import { ProviderSeed } from '@waves/provider-seed';
+
+// // const seed = libs.crypto.randomSeed(15);
+// // const seed = libs.crypto.Seed.toString();
+// const signer = new Signer({
+//   // Specify URL of the node on Testnet
+//   NODE_URL: 'https://nodes-testnet.wavesnodes.com',
+//   LOG_LEVEL: 'verbose'
+// });
+// signer.setProvider(new ProviderSeed());
+
+// export {
+//   signer
+// }
